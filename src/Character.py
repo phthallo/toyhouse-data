adfd112c06fc6798aaffb08fbfe5926a0afa05bc
phthallo <84078890+phthallo@users.noreply.github.com>
2023-10-23 14:23:05 +1030

    Add character log and (some) character stats

--- src/Character.py
+++ src/Character.py
@@ -18,14 +18,42 @@ class Character:
         """
         self.id = id
         self.session = session.session
-        self.char_stats = []
+        self.char_stats = {}
+        self.ownership_log = []
         self.favs = []
         self.comments = []
 
     def retrieve_char_stats(self):
         """
-        Obtains information such as character designer, date of creation and ownership log.
+        Obtains information such as:
+        - designer and creator of the character (if applicable, considering designer is an optinal field)
+        - the date of creation
+        - the number of Gallery, Library, World and Link items (left sidebar) [wip]
+        - the number of comments and favourites (left sidebar underneath) [wip]
         """
+        retrieve_char_stat_attributes = scrape(self.session, f"https://toyhou.se/{self.id}./", "dt", {"class": "field-title col-sm-4"})
+        retrieve_char_stat_values = scrape(self.session, f"https://toyhou.se/{self.id}./", "dd", {"class": "field-value col-sm-8"})
+        created_date = scrape(self.session, f"https://toyhou.se/{self.id}./", "abbr", {"class": "tooltipster datetime"}, all=False)["title"]
+        retrieve_char_stat_values[0] = created_date
+        for attribute, value in zip(retrieve_char_stat_attributes, retrieve_char_stat_values):
+            try:
+                self.char_stats[(attribute.text).strip()] = (value.text).strip()
+            except:
+                self.char_stats[(attribute.text).strip()] = (value).strip()        
+        return self.char_stats
+    
+    def retrieve_char_log(self):
+        """
+        Obtains the ownership log of the character.
+        """
+        retrieve_char_transfer_date = scrape(self.session, f"https://toyhou.se/{self.id}./ownership/log", "td", {"class": "col-4 col-md-3"})
+        retrieve_char_recipient = scrape(self.session, f"https://toyhou.se/{self.id}./ownership/log", "span", {"class": "display-user"})
+        for date, recipient in zip(retrieve_char_transfer_date, retrieve_char_recipient[1:]):
+            self.ownership_log.append({
+                "transfer_date": (date.text).strip(),
+                "transfer_recipient": (recipient.text).strip()
+            })
+        return self.ownership_log
 
     def retrieve_favs(self):
         """
--- src/utilities.py
+++ src/utilities.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-def scrape(session, url, tag, class_attr):
+def scrape(session, url, tag, class_attr, all=True):
     retrieve_url = session.get(url)
     retrieve_lxml = BeautifulSoup(retrieve_url.content, features="lxml")
-    retrieve_attributes = retrieve_lxml.find_all(tag, class_attr)
+    if all:
+        retrieve_attributes = retrieve_lxml.find_all(tag, class_attr)
+    else: 
+        retrieve_attributes = retrieve_lxml.find(tag, class_attr)
     return retrieve_attributes
